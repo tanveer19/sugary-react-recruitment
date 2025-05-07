f5994444a8c633b03134c31f042b25986c9bb810
tanveer19 <thjbd19@gmail.com>
2025-05-07 07:22:51 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,99 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Header = () => {
-  return <div>Header</div>;
+  // const { user, logOut } = useContext(AuthContext);
+  const handleLogout = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.error(error));
+  };
+
+  const navOptions = (
+    <>
+      <Link to="/">
+        {/* <img src="/images/logo.png" className="w-16" alt="" /> */}
+      </Link>
+      <li>
+        <Link to="/">
+          <h3 className="font-bold text-2xl">Sugary</h3>
+        </Link>
+      </li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+
+      {/* {user && (
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      )}
+
+      {user && (
+        <li>
+          <Link to="/dashboard/userhome">Dashboard</Link>
+        </li>
+      )}
+
+      {user && (
+        <div>
+          <img src={user.photoURL} alt="" style={{ width: "60px" }} />
+        </div>
+      )}
+
+      <li>
+        {user ? (
+          <>
+            <span>{user.email}</span>
+            <button
+              onClick={handleLogout}
+              className="btn btn-xs self-center w-fit"
+            >
+              Sign out
+            </button>
+          </>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </li> */}
+    </>
+  );
+  return (
+    <div className="navbar md:justify-center max-w-7xl mx-auto">
+      {/* mobile */}
+      <div className="navbar-start sm:hidden">
+        <div className="dropdown z-10">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {navOptions}
+          </ul>
+        </div>
+      </div>
+      {/* desktop */}
+      <div className="navbar-center hidden lg:flex ">
+        <ul className="menu menu-horizontal px-1 items-center">{navOptions}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,92 @@
+import React, { createContext, useEffect, useState } from "react";
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import axios from "axios";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+  // observe auth state change
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      // console.log("current user", currentUser);
+
+      // get and set token
+      if (currentUser) {
+        axios
+          .post("https://2-21-a12-summer-camp-server.vercel.app/jwt", {
+            email: currentUser.email,
+          })
+          .then((data) => {
+            // console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+            setLoading(false);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    googleSignIn,
+    logOut,
+    updateUserProfile,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
