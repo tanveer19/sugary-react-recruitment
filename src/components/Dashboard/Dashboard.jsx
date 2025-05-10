609fd4f3055b7b06a18c66db8214e018c8726f29
tanveer19 <thjbd19@gmail.com>
2025-05-11 05:42:56 +0600

    t

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../../providers/AuthProvider";
 import { useState } from "react";
 import { useEffect } from "react";
 import axiosInstance from "../hooks/axiosInstance";
+import LazyLoad from "react-lazy-load";
 
 const Dashboard = () => {
   const { accessToken } = useContext(AuthContext);
@@ -44,15 +45,19 @@ const Dashboard = () => {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {materials.map((item) => (
-            <div key={item.Id} className="border rounded shadow">
-              <img
-                src={`https://d1wh1xji6f82aw.cloudfront.net/${item.CoverPhoto}`}
-                alt={item.title}
-                className="w-full h-40 object-cover mb-2"
-              />
-              <h3 className="font-semibold">{item.Title}</h3>
-              <p className="text-sm">{item.BrandName}</p>
-              <p className="font-bold">{item.SalesPriceInUsd.toFixed(2)}</p>
+            <div key={item.Id} className="border-2 rounded shadow ">
+              <LazyLoad height={160} offset={100}>
+                <img
+                  src={`https://d1wh1xji6f82aw.cloudfront.net/${item.CoverPhoto}`}
+                  alt={item.title}
+                  className="w-full h-40 object-cover mb-2"
+                />
+              </LazyLoad>
+              <div className="p-2">
+                <h3 className="font-semibold">{item.Title}</h3>
+                <p className="text-sm">{item.BrandName}</p>
+                <p className="font-bold">{item.SalesPriceInUsd.toFixed(2)}</p>
+              </div>
             </div>
           ))}
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>I am Homepage </div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-screen p-4">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Sugary!</h1>
+        <p className="mb-6 text-lg text-center max-w-xl">
+          Discover and manage materials easily. Login to access your dashboard
+          and explore more features.
+        </p>
+        <Link to="/login" className="btn btn-primary">
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
